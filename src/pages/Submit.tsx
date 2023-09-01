ec5d6922a064ec18a7b8d31bd165ac787dcb2a2e
codexsusan <susankhadka708@gmail.com>
2023-09-01 09:18:43 +0530

    Show message for post submission

--- src/pages/Submit.tsx
+++ src/pages/Submit.tsx
@@ -6,16 +6,18 @@ import { Separator } from "@/components/ui/separator";
 import { changeCommunity, createPost } from "@/features/submit/submitSlice";
 import useDocumentTitle from "@/hooks/useDocumentTitle";
 import { Loader2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Submit() {
   useDocumentTitle("Submit | Social Hub");
   const dispatch = useAppDispatch();
   const submit = useAppSelector((state) => state.submit);
+  const navigate = useNavigate();
 
   const handlePostSubmit = () => {
     dispatch(createPost(submit.post)).then((res) => {
       if (res.type === "submit/createPost/fulfilled") {
-        console.log(res);
+        navigate("/");
       }
     });
   };
