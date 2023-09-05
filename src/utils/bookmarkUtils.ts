b74311d0a883c2b6e298d0f5d5ade742fa8e0b39
codexsusan <susankhadka708@gmail.com>
2023-09-05 14:53:49 +0530

    Created Bookmark Utils

---
+++ src/utils/bookmarkUtils.ts
@@ -0,0 +1,24 @@
+import { PostPartial } from "@/types/postTypes";
+import { request } from "./httpUtils";
+
+// Add Bookmark
+export const addBookmarkUtils = (postId: PostPartial["_id"]) => {
+  const response = request(`/api/bookmarks/add-bookmark/${postId}`, "POST", {});
+  return response;
+};
+
+// Get Bookmarks
+export const getBookmarksUtils = () => {
+  const response = request("/api/bookmarks/get-bookmarks", "GET");
+  return response;
+};
+
+// Remove Bookmark
+export const removeBookmarkUtils = (postId: PostPartial["_id"]) => {
+  const response = request(
+    `/api/bookmarks/remove-bookmark/${postId}`,
+    "DELETE",
+    {}
+  );
+  return response;
+};
