02a2a3944be5f2c80e178befb47a9648bd6754b1
Hritik <hritikadhikari05@gmail.com>
2023-10-27 23:59:45 +0530

    Added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import AppRouter from "./router/AppRouter";
-import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
-      <Analytics />
       <AppRouter />
     </>
   );
